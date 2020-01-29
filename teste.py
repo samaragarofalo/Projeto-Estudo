d2ef62d8be04059c3535452d0f4db0b2ba4eca7b
samaragarofalo <samaragarofalo@icloud.com>
2020-01-29 16:24:45 -0200

    Alteração no print

--- teste.py
+++ teste.py
@@ -1,1 +1,7 @@
-print("hello world")
+resp=input('Voce quer morrer hoje?')
+if resp=='sim':
+    print('eu tbm')
+elif resp=='nao':
+    print('que pena, eu quero')
+else:
+    print('resposta invalida animal')
